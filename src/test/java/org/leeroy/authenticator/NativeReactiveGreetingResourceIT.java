0353619624e4749f4720d141c2be6ba9f575c1be
User <j.a.castanheira@gmail.com>
2021-11-23 11:16:40 +0000

    Initial commit

---
+++ src/test/java/org/leeroy/authenticator/NativeReactiveGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package org.leeroy.authenticator;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeReactiveGreetingResourceIT extends ReactiveGreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
