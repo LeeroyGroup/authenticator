d0df2dce999bfdc740c0b738cd36a7533d019a0a
User <j.a.castanheira@gmail.com>
2021-11-23 15:45:10 +0000

    ReactivePanacheMongoRepository

--- src/main/java/org/leeroy/authenticator/repository/AccountRepository.java
+++ src/main/java/org/leeroy/authenticator/repository/AccountRepository.java
@@ -1,10 +1,10 @@
 package org.leeroy.authenticator.repository;
 
-import io.quarkus.mongodb.panache.PanacheMongoRepository;
+import io.quarkus.mongodb.panache.reactive.ReactivePanacheMongoRepository;
 import org.leeroy.authenticator.model.Account;
 
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-public class AccountRepository implements PanacheMongoRepository<Account> {
+public class AccountRepository implements ReactivePanacheMongoRepository<Account> {
 }
--- src/main/java/org/leeroy/authenticator/repository/BlockedIPRepository.java
+++ src/main/java/org/leeroy/authenticator/repository/BlockedIPRepository.java
@@ -1,10 +1,10 @@
 package org.leeroy.authenticator.repository;
 
-import io.quarkus.mongodb.panache.PanacheMongoRepository;
+import io.quarkus.mongodb.panache.reactive.ReactivePanacheMongoRepository;
 import org.leeroy.authenticator.model.BlockedIP;
 
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
-public class BlockedIPRepository implements PanacheMongoRepository<BlockedIP> {
+public class BlockedIPRepository implements ReactivePanacheMongoRepository<BlockedIP> {
 }
