2666e21a60201c982c63673fdc596af2fba29541
User <j.a.castanheira@gmail.com>
2021-11-24 15:35:38 +0000

    BlockedAccessService method blockIP

--- src/main/java/org/leeroy/authenticator/service/impl/BlockedAccessServiceImpl.java
+++ src/main/java/org/leeroy/authenticator/service/impl/BlockedAccessServiceImpl.java
@@ -32,6 +32,6 @@ public class BlockedAccessServiceImpl implements BlockedAccessService {
 
     @Override
     public void blockIP(BlockedAccess blockedAccess) {
-
+        blockedAccessRepository.persist(blockedAccess);
     }
 }
